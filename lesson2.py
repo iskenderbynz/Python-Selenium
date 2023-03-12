002aa56b792208f613ee887649386bd8c7ef0f99
iskenderbynz <iskenderboynuz90@gmail.com>
2023-03-12 22:52:31 +0300

    Create lesson2.py
    Lesson2 upload

---
+++ lesson2.py
@@ -0,0 +1,50 @@
+database = []
+
+def newCreate():
+    database.append(input("Ad soyad giriniz : "))
+    menu()
+
+def lists():
+    for index, i in enumerate(database):
+        print(index," - ", i) 
+    menu()
+
+def delete():
+    bul = input("Silmek istediğiniz kaydı belirtiniz : ")
+    database.remove(bul)
+    menu()
+
+def find():
+    bul = input("Numarasını öğrenmek istediğiniz öğrenciyi yazınız : ")
+    print(database.index(bul))
+    menu()
+
+def findAll():
+    deleteDb = []
+    deleteDb = input("Toplu kayıt silmek için öğrenci numaralarını virgül (,) ile ayırarak belirtiniz.: ").split(",")
+    i = 0
+    while i < len(deleteDb):
+        database.pop(int(deleteDb[i]))
+        i = i + 1
+    menu()
+
+def menu():
+    print("1 : Yeni öğrenci ekle")
+    print("2 : Kayıtları listele")
+    print("3 : Kayıt sil")
+    print("4 : Toplu kayıt sil")
+    print("5 : Öğrenci numarası öğren")
+    islem = input("Yapmak istediğiniz işlemi belirtiniz : ")
+    if islem == "1":
+        newCreate()
+    elif islem == "2":
+        lists()
+    elif islem == "3":
+        delete()
+    elif islem == "4":
+        findAll()
+    elif islem == "5":
+        find()
+   
+menu()
+
