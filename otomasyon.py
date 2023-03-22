99a642363533c5bd0dae4759a8d72af166b01fea
iskenderbynz <iskenderboynuz90@gmail.com>
2023-03-22 15:39:24 +0300

    Update otomasyon.py

--- otomasyon.py
+++ otomasyon.py
@@ -2,9 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-
+testRaporlari = [0,0,0,0,0,0]
 menuSec = ""
-
 def menu():
       print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
       print("1 - Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak 'Epic sadface: Username is required' gösterilmelidir.")
@@ -36,6 +35,18 @@ def menu():
             testClass.login_control4(7)
             testClass.login_control5(7)
             testClass.login_control6(7)
+            print("Senaryolar ve başarı durum raporları aşağıdaki gibidir;")
+            i = 1
+            bsenaryo = 0
+            hsenaryo =0
+            for senaryo in testRaporlari:
+                  if senaryo==True:
+                        bsenaryo +=1
+                  elif senaryo==False:
+                        hsenaryo +=1
+                  i +=1
+            print(f"Toplam {i} senaryo koşulmuştur. {bsenaryo} adet senaryo başarılı, {hsenaryo} adet senaryo başarısız olacak şekilde tamamlanmıştır.")
+            menu()
       if menuSec=="8":
             quit()
 
@@ -49,6 +60,7 @@ class Test_Kodlamaio:
         loginBtn.click()
         errorMessage = driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Username is required"
+        testRaporlari[0] = testResult
         print(f"Senaryo 1 - Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak 'Epic sadface: Username is required' gösterilmelidir. : {testResult}")
         driver.close()
         if menuSec != 7:
@@ -66,6 +78,7 @@ class Test_Kodlamaio:
             loginBtn.click()
             errorMessage = driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
             testResult = errorMessage.text == "Epic sadface: Password is required"
+            testRaporlari[1] = testResult
             print(f"Senaryo 2 - Sadece şifre alanı boş geçildiğinde uyarı mesajı olarak 'Epic sadface: Password is required' gösterilmelidir. : {testResult}")
             driver.close()
             if menuSec != 7:
@@ -83,6 +96,7 @@ class Test_Kodlamaio:
             loginBtn.click()
             errorMessage = driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
             testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+            testRaporlari[2] = testResult
             print(f"Senaryo 3 - Kullanıcı adı 'locked_out_user' şifre alanı 'secret_sauce' gönderildiğinde 'Epic sadface: Sorry, this user has been locked out.' mesajı gösterilmelidir. : {testResult}")
             driver.close()
             if menuSec != 7:
@@ -105,6 +119,7 @@ class Test_Kodlamaio:
                 inputIcon = driver.find_elements(By.XPATH,"//*[name()='svg']")
             
             testResult = len(inputIcon) == 0
+            testRaporlari[3] = testResult
             print(f"Senaryo 4 - Kullanıcı adı ve şifre alanları boş geçildiğinde bu iki inputun yanında da kırmızı 'X' ikonu çıkmalıdır. Daha sonra aşağıda çıkan uyarı mesajının kapatma butonuna tıklandığında bu 'X' ikonları kaybolmalıdır. : {testResult}")
             driver.close()
             if menuSec != 7:
@@ -121,8 +136,9 @@ class Test_Kodlamaio:
             loginBtn = driver.find_element(By.ID,"login-button")
             loginBtn.click()
             pageActive = driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div[2]/span")
-            pageTitle = pageActive.text == "Products"
-            print(f"Senaryo 5 - Kullanıcı adı 'standard_user' şifre 'secret_sauce' gönderildiğinde kullanıcı '/inventory.html' sayfasına gönderilmelidir. : {pageTitle}")
+            testResult = pageActive.text == "Products"
+            testRaporlari[4] = testResult
+            print(f"Senaryo 5 - Kullanıcı adı 'standard_user' şifre 'secret_sauce' gönderildiğinde kullanıcı '/inventory.html' sayfasına gönderilmelidir. : {testResult}")
             driver.close()
             if menuSec != 7:
                 menu()
@@ -139,6 +155,7 @@ class Test_Kodlamaio:
             loginBtn.click()
             productCount = driver.find_elements(By.CLASS_NAME,"inventory_item_name")
             testResult = len(productCount) == 6
+            testRaporlari[5] = testResult
             print(f"Senaryo 6 - Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı '6' adet olmalıdır. : {testResult}")
             driver.close()
             if menuSec != 7:
